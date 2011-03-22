d60b99f8887428cfdd44aa2791c1686e8bb8418a
Marius Grigaitis <admin@mar.lt>
2011-03-22 18:13:21 +0200

    Moved spec to special directory

--- objects/team_spec.rb
+++
@@ -1,7 +0,0 @@
-require "./team"
-
-describe Team do
-    it "initializes" do
-        team = Team.new
-    end
-end
--- spec/objects/team_spec.rb
+++ spec/objects/team_spec.rb
@@ -5,21 +5,23 @@ require "./objects/team_member"
 describe Team do
     before (:each) do
         @team = Team.new
+        @person = Person.new
     end
 
     it "should have no members when new team is created" do
-        @team.members.should have(0).things
+        @team.members.should be_empty
     end
 
     it "should have 1 team member after adding member" do
-        @team.add_member(Person.new)
+        @team.add_member(@person)
 
         @team.members.should have(1).things
         @team.members[0].should be_instance_of(TeamMember)
     end
 
-    it "should have member pointing to current team after adding member" do
-        @team.add_member(Person.new)
+    it "should have member pointing to current team and added person" do
+        @team.add_member(@person)
         @team.members[0].team.should == @team
+        @team.members[0].person.should == @person
     end
 end
